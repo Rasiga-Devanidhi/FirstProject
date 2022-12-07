a820741cac9057bfa306b401fa3adc0c61c5a9cb
Rasiga-Devanidhi <65544708+Rasiga-Devanidhi@users.noreply.github.com>
2022-12-07 15:34:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,44 @@
-using BWebApplication5;
-using WebApplication5.DataAccess;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-//Changes made in GitHub 
-AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
-builder.Services.AddDefaultAWSOptions(awsOptions);
-
-
-
-builder.Services.AddScoped<IDataAccessProvider, DataAccessDynamoDBProvider>();
-Task t = DynamoDBInitializer.InitializeDynamoDB();
-t.Wait();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 app.UseAuthorization();
-
 app.MapControllers();
+app.Run();
 
-app.stop();
+
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
